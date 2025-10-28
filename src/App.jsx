29cd86d234ccda5034a631bff5a24616c5f46d8d
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-10-28 18:21:22 +0530

    Lenis Applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import Messages from './Pages/Messages'
+import React, { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
+import Messages from "./Pages/Messages";
 
 const App = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.3,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
+      smoothWheel: true, 
+      smoothTouch: true,
+      direction: "vertical",
+      gestureDirection: "vertical",
+    });
+
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
-    <div className='bg-black'>
+    <div className="bg-black min-h-screen text-white">
       <Messages />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
